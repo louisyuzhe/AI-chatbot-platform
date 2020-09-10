e6e0e5bbf58ce136036c0fab77abe3141f972489
louisyuzhe <louisyuzhe@gmail.com>
2020-09-10 01:09:06 -0500

    Add hidden credential file and load credentiall into settings

--- webpage/webpage/settings.py
+++ webpage/webpage/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from credentials import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^mbgkkexd4$4=ijqdp#v1^m)u8&62+u%t^_k=m)+1^-3mza!@c'
+SECRET_KEY = credentials.django_secretKey
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
