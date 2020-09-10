fd7dc0563fd0ec284d1aa4d6ac4bd123a3062c7f
louisyuzhe <louisyuzhe@gmail.com>
2020-09-10 00:43:09 -0500

    Modify setting to setup for basic html

--- webpage/blog/views.py
+++ webpage/blog/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-#from blog.models import posts
+from blog.models import posts
 
 # Create your views here.
  
 def home(request):
-    return render(request, 'index.html')
+    """
+    content = {
+        'title' : 'My First Post',
+        'author' : 'Giles',
+        'date' : '18th September 2011',
+        'body' : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam cursus tempus dui, ut vulputate nisl eleifend eget. Aenean justo felis, dapibus quis vulputate at, porta et dolor. Praesent enim libero, malesuada nec vestibulum vitae, fermentum nec ligula. Etiam eget convallis turpis. Donec non sem justo.',
+    }
+    """
+    entries = posts.objects.all()[:10]
+    return render(request, 'index.html', {'posts' : entries})
