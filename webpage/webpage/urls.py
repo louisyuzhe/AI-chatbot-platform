75ce8e95843c02953892ffb1fc09926930039f13
louisyuzhe <louisyuzhe@gmail.com>
2020-09-14 00:57:23 -0500

    Add chatbot and get-response page in View to path

--- webpage/webpage/urls.py
+++ webpage/webpage/urls.py
@@ -16,7 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from blog import views
+
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('admin/', admin.site.urls),
+    path('get-response/', views.get_response),
+    path('chatbot/', views.chatbot),
 ]
+
+if settings.DEBUG == True:
+	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
